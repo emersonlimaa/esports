6c4596c4b4c612756bf73cf04d7a84390930e972
Emerson Lima <emerson.dev7@gmail.com>
2022-09-28 22:10:00 -0300

    add prisma server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
-app.get('/ads', (request, response) =>{
+app.get('/games', (request, response) => {
+  return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+})
+
+app.get('/ads/:id/discord', (request, response) =>{
+
+  // const gameId = request.params.id;
+
+
   return response.json([
     { id: 1, name: "Anuncio 1"},
     { id: 2, name: "Anuncio 2"},
